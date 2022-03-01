6cce26f2e5292f82fe41d7f1ab1eb7b70aca9a40
vifas <victorfaustino12@gmail.com>
2022-03-01 14:32:04 -0300

    react

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 
+function Home() {
+    return (
+    <div>
+        <h1>Home</h1>
+        <Contador/>
+    </div>
+    )
+}
+
 function Contador() {
     const [contador,setContador] = useState(1);
 
     function adicionarContador() {
         setContador(contador + 1);
     }
-
     return (
         <div>
             <div>{contador}</div>
@@ -15,6 +23,4 @@ function Contador() {
     )
 }
 
-function Pagina() {
-    return <Contador />
-}
+export default Home
