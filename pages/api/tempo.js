c79c23c84a5c9b12a004a5c1adee1a6d6e05e725
vifas <99108153+vifas@users.noreply.github.com>
2022-07-14 13:45:57 -0300

    adicionar rota /api/tempo

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,9 @@
-function tempo(reques, response) {
+function tempo(request, response) {
     const dynamicDate = new Date();
 
     response.json({
         date: dynamicDate.toGMTString()
-    })
+    });
 }
 
 export default tempo;
